43de4b040398f085c11e99c9e12efe7e04f525ec
latelyhappy <39298306+latelyhappy@users.noreply.github.com>
2026-03-10 17:09:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import threading, warnings
 from flask import Flask, jsonify, render_template
 from flask_cors import CORS
-
-# 載入我們拆分出來的模組
 import config
 from scanner import scanner_engine
 
@@ -11,18 +9,12 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route('/data')
-def get_data(): 
-    return jsonify(config.MASTER_BRAIN)
+def get_data(): return jsonify(config.MASTER_BRAIN)
 
 @app.route('/')
-def index(): 
-    # Flask 會自動去 templates 資料夾底下找 index.html
-    return render_template('index.html')
+def index(): return render_template('index.html')
 
 if __name__ == '__main__':
-    # 啟動後台股票掃描大腦
     threading.Thread(target=scanner_engine, daemon=True).start()
-    
-    # 啟動前端網頁伺服器
     print("🌐 網頁伺服器啟動！請在瀏覽器輸入: http://127.0.0.1:8080")
     app.run(host='0.0.0.0', port=8080)
